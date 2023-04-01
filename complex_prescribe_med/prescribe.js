71bf9b794fc694a33286531c188f48f6b3eb3298
tshanmei <112236867+tshanmei@users.noreply.github.com>
2023-04-01 17:33:44 +0800

    prescribe

--- complex_prescribe_med/prescribe.js
+++ complex_prescribe_med/prescribe.js
@@ -148,6 +148,8 @@ const root = Vue.createApp({
             months: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
 
             datetosearch: '',
+
+            medicine_fields: [],
         }
     },
     // mounted() {
@@ -185,69 +187,98 @@ const root = Vue.createApp({
         //     console.log(this.date_time[0])
             
         // },
-        addMedicineFields() {
-            event.preventDefault()
-            console.log("add fields called")
-            // this.medicinecount += 1
+        // addMedicineFields() {
+        //     event.preventDefault()
+        //     console.log("add fields called")
+        //     // this.medicinecount += 1
 
-            // var medicinefields = document.getElementById("medicine-fields")
-            // medicinefields.innerHTML += `
-            //     <label for="medicine-${this.medicinecount}">Medicine:</label>
-            //     <select id="medicine-${this.medicinecount}" v-model="selected_drug">
-            //         <option value="'" disabled selected>Choose Drug</option>
-            //         <option v-for="drug in drugs" v-bind:value="drug" >{{ drug }}</option>
-            //     </select><br>
-            //     <label for="frequency-${this.medicinecount}">Frequency:</label>
-            //     <input type="text" id="frequency-${this.medicinecount}" name="frequency[]"><br>
-            //     <label for="amount-${this.medicinecount}">Amount:</label>
-            //     <input type="text" id="amount-1" name="amount[]"><br>
-            // `
-            this.medicineCount++
-            this.medicines.push({ id: this.medicineCount })
+        //     // var medicinefields = document.getElementById("medicine-fields")
+        //     // medicinefields.innerHTML += `
+        //     //     <label for="medicine-${this.medicinecount}">Medicine:</label>
+        //     //     <select id="medicine-${this.medicinecount}" v-model="selected_drug">
+        //     //         <option value="'" disabled selected>Choose Drug</option>
+        //     //         <option v-for="drug in drugs" v-bind:value="drug" >{{ drug }}</option>
+        //     //     </select><br>
+        //     //     <label for="frequency-${this.medicinecount}">Frequency:</label>
+        //     //     <input type="text" id="frequency-${this.medicinecount}" name="frequency[]"><br>
+        //     //     <label for="amount-${this.medicinecount}">Amount:</label>
+        //     //     <input type="text" id="amount-1" name="amount[]"><br>
+        //     // `
+        //     this.medicineCount++
+        //     this.medicines.push({ id: this.medicineCount })
+        //     console.log(this.medicine_fields)
+        //   },
+        addMedicineFields() {
+            event.preventDefault();
+            console.log("add fields called");
+            // Get the parent container for the input fields
+            var medicineFieldsContainer = document.querySelector("#medicine-fields");
+            // Get the last input field set
+            var lastMedicineFields = medicineFieldsContainer.childNodes[medicineFieldsContainer.childNodes.length - 2];
+            // Clone the last input field set
+            var newMedicineFields = lastMedicineFields.cloneNode(true);
+            // Clear the ID attributes of the cloned input fields to avoid duplicates
+            newMedicineFields.querySelectorAll("input, select").forEach((el) => {
+                el.removeAttribute("id");
+                el.value = ""; // reset the value to empty string
+            });
+            // Append the cloned input fields to the parent container
+            medicineFieldsContainer.appendChild(newMedicineFields);
           },
         removeMedicineFields(index) {
             console.log("remove fields called")
             this.medicines.splice(index, 1)
         },
         prescribeMedicine() {
-            var patientId = document.getElementById("patient-id").value;
+            let patientId = sessionStorage.getItem('id')
             var prescriptionDetails = [];
-            var medicineFields = document.getElementsByClassName("medicine-field");
+            var medicineFields = document.querySelectorAll('.medicine-field');
+            console.log(medicineFields)
             for (var i = 0; i < medicineFields.length; i++) {
-                var medicine = {
-                    med_name: medicineFields[i].querySelector('input[name="medicine[]"]').value,
-                    frequency: medicineFields[i].querySelector('input[name="frequency[]"]').value,
-                    amount: medicineFields[i].querySelector('input[name="amount[]"]').value
-                };
-                prescriptionDetails.push(medicine);
+              var medicineNameInput = medicineFields[i].querySelector('select[id^="medicine-"]');
+              var frequencyInput = medicineFields[i].querySelector('input[name="frequency[]"]');
+              var amountInput = medicineFields[i].querySelector('input[name="amount[]"]');
+              // Add error checking
+              if (!medicineNameInput || !frequencyInput || !amountInput) {
+                console.log('Error: missing input field');
+              }
+              console.log("Medicine name input:", medicineNameInput);
+              console.log("Frequency input:", frequencyInput);
+              console.log("Amount input:", amountInput);
+          
+              var medicine = {
+                med_name: medicineNameInput.value,
+                frequency: frequencyInput.value,
+                amount: amountInput.value
+              };
+              console.log(medicine)
+              prescriptionDetails.push(medicine);
             }
+            console.log("this is the prescription details" + JSON.stringify(prescriptionDetails))
             $.ajax({
-                url: "http://127.0.0.1:5101/prescribe_medicine/",
-                type: "POST",
-                data: JSON.stringify({
-                    patient_id: patientId,
-                    prescription_details: prescriptionDetails,
-                    // apptdate
-                }),
-                contentType: "application/json",
-                dataType: "json",
-                success: function(data) {
-                    console.log("Flask responding", data);
-                    if (data.code === 400 || data.code === 422) {
-                        // Display error message
-                        var errorMessage = data.message;
-                        $("#error-messages").html(errorMessage);
-                    }
-                    else if (data.code === 200){
-                        var showMessage = data.message;
-                        $("#error-messages").html(showMessage);
-                    }
-                },
-                error: function(xhr, status, error) {
-                    console.log("Error saving prescription:", xhr.responseText);
+              url: "http://127.0.0.1:5101/prescribe_medicine/",
+              type: "POST",
+              data: JSON.stringify({
+                patient_id: patientId,
+                prescription_details: prescriptionDetails,
+              }),
+              contentType: "application/json",
+              dataType: "json",
+              success: function(data) {
+                console.log("Flask responding", data);
+                if (data.code === 400 || data.code === 422) {
+                  var errorMessage = data.message;
+                  $("#error-messages").html(errorMessage);
+                } else if (data.code === 200) {
+                  var showMessage = data.message;
+                  $("#error-messages").html(showMessage);
                 }
+              },
+              error: function(xhr, status, error) {
+                console.log("Error saving prescription:", xhr.responseText);
+              }
             })
-        }
+          },
     },
 
     
@@ -282,19 +313,19 @@ const root = Vue.createApp({
 root.component('medicine-field', {
     props: ['medicine', 'index', 'drugs'],
     template: `
-      <div class="medicine-field">
-        <label :for="'medicine-' + index">Medicine:</label>
-        <select :id="'medicine-' + index" v-model="medicine.selected_drug">
-          <option value="" disabled selected>Choose Drug</option>
-          <option v-for="drug in drugs" :value="drug">{{ drug }}</option>
-        </select>
-        <button class="btn btn-warning" @click="$emit('remove-medicine')">Remove</button>
-        <br>
-        <label :for="'frequency-' + index">Frequency:</label>
-        <input type="text" :id="'frequency-' + index" name="frequency[]"><br>
-        <label :for="'amount-' + index">Amount:</label>
-        <input type="text" :id="'amount-' + index" name="amount[]"><br>
-      </div>
+    <div class="medicine-field">
+    <label :for="'medicine-' + index">Medicine:</label>
+    <select :id="'medicine-' + index" v-model="medicine.selected_drug">
+      <option value="" disabled selected>Choose Drug</option>
+      <option v-for="drug in drugs" :value="drug">{{ drug }}</option>
+    </select>
+    <button class="btn btn-warning" @click="$emit('remove-medicine')">Remove</button>
+    <br>
+    <label :for="'frequency-' + index">Frequency:</label>
+    <input type="text" :id="'frequency-' + index" name="frequency[]"><br>
+    <label :for="'amount-' + index">Amount:</label>
+    <input type="text" :id="'amount-' + index" name="amount[]"><br>
+  </div>
     `
   })
 root.mount("#prescription")
